d78c4780e03e2c1006dc732a9283f6cfa98b30a7
geetha2705 <2100031372@kluniversity.in>
2024-06-07 11:17:31 +0530

    Finished
    Date:07-06-2024

---
+++ Data.js
@@ -0,0 +1,38 @@
+let allmusic = [
+{
+name:'kalalo kooda', 
+artist:'Vijay devarakonda',
+img:'image1', 
+src:'music1'      
+ }, 
+ 
+{
+name:'Evareevaro', 
+artist:'Ranbir',
+img:'image2',
+src:'music2'
+},
+{
+name:'Kaanunna Kalyanam', 
+artist:'Dulquer Salman',
+img:'image3',
+src:'music3'
+},
+{
+name: 'Priyathama Priyathama',
+artist: 'NagaChaitanya',
+img: 'image4',
+src: 'music4'
+}, 
+{
+   name : 'Ra ra reddy',
+   artist : 'Nithin',
+   img : 'image5',
+   src : 'music5'
+
+},
+
+
+]
+
+//You can add more songs like this
